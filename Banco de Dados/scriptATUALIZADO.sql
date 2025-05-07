bb15de71bb0657a2e27f1f4e199e58e8bd6d05c5
RenanDaher1 <renan.franceschelli@sptech.school>
2025-05-07 18:14:21 -0300

    Subindo atualizações do projeto individual

---
+++ Banco de Dados/scriptATUALIZADO.sql
@@ -0,0 +1,24 @@
+create database ti;
+use ti;
+
+create table usuario(
+idUsuario int primary key auto_increment,
+nome varchar(45) not null,
+email varchar(45) not null,
+nome_usuario varchar(45) unique not null,
+senha varchar(45) unique not null);
+
+create table aula(
+idAula int primary key auto_increment,
+titulo varchar(45) unique not null,
+dtAula datetime not null);
+
+create table usuario_aula(
+id int auto_increment not null,
+fkAula int not null,
+fkUsuario int not null,
+dtInicio_aula datetime not null,
+primary key (id, fkAula, fkUsuario),
+foreign key (fkUsuario) references usuario(idusuario),
+foreign key (fkAula) references aula(idAula));
+
