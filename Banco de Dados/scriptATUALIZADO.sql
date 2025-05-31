536087969c5a9f25743e3945c406b7a1bdc2adee
RenanDaher1 <renan.franceschelli@sptech.school>
2025-05-31 20:29:28 -0300

    atualização do MER  e script no banco de dados e começo de atribuição e requisição dos valores para  colocar na dashboard

--- Banco de Dados/scriptATUALIZADO.sql
+++ Banco de Dados/scriptATUALIZADO.sql
@@ -10,8 +10,8 @@ senha varchar(45) unique not null);
 
 create table aula(
 idAula int primary key auto_increment,
-titulo varchar(45) unique not null,
-dtAula datetime not null);
+modulo int,
+titulo varchar(200) unique not null);
 
 create table usuario_aula(
 id int auto_increment not null,
@@ -22,5 +22,9 @@ primary key (id, fkAula, fkUsuario),
 foreign key (fkUsuario) references usuario(idusuario),
 foreign key (fkAula) references aula(idAula));
 
+-- insert into aula values
+-- (default, '1- O que é educação financeira e por que ela importa?', '1');
+
 select * from usuario;
+
 
